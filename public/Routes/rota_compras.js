03a13c7a27d43aa985d2517f81175297e1bfad80
Diego Santana <diego.alves.santana@hotmail.com>
2023-06-15 00:15:41 -0300

    Lixeira na pagina Carrinho
    Foi adicionado uma lixeira na página do carrinho, para que fosse possível remover itens do carrinho.

--- public/Routes/rota_compras.js
+++ public/Routes/rota_compras.js
@@ -12,15 +12,26 @@ const correios = new Correios();
 router.get('/', (req,res)=>{
     let carrinho = req.session.carrinho
     if(carrinho){
-        Itens.findAll({where: {ITM_IdItem: {[Op.in]: carrinho}}}).then((itens)=>{
+        if (req.session.carrinho.length !==0 ){
+            Itens.findAll({where: {ITM_IdItem: {[Op.in]: carrinho}}}).then((itens)=>{
+                if(req.session.usuario){
+                    var usuarioSessao = req.session.usuario
+                    res.render('carrinho', {itens: itens, usuario: usuarioSessao})
+                }else{
+                    let redirectUrl = '/carrinho'
+                    res.render('login', {redirectUrl: redirectUrl});
+                }
+            })
+        }else{
             if(req.session.usuario){
                 var usuarioSessao = req.session.usuario
-                res.render('carrinho', {itens: itens, usuario: usuarioSessao})
+                console.log('foi para a página da mensagem com Usuario')
+                res.render('mensagens', {usuario: usuarioSessao})
             }else{
-                let redirectUrl = '/carrinho'
-                res.render('login', {redirectUrl: redirectUrl});
+                console.log('foi para a página da mensagem')
+                res.redirect('/carrinho/mensagem')
             }
-        })
+        }
     }else{
         if(req.session.usuario){
             var usuarioSessao = req.session.usuario
@@ -81,6 +92,25 @@ function calcularFrete(cep) {
 
 router.get('/mensagem', (req,res)=>{
     res.render('mensagens')
+})
+
+router.get('/remove_item/:item_id', (req,res)=>{
+    const item_id = parseInt(req.params.item_id); // ID do item a ser removido
+    console.log(item_id)
+    if (req.session.carrinho) {
+        const carrinho = req.session.carrinho;
+        console.log(carrinho)
+        // Verifica se o ID do item está presente no carrinho
+        const index = carrinho.indexOf(item_id);
+        console.log(index)
+      if (index > -1) {
+        // Remove o ID do item do carrinho usando splice
+        carrinho.splice(index, 1);
+        console.log(carrinho)
+      }
+    }
+  
+    res.redirect('/carrinho');
 })
 
 module.exports= router;
