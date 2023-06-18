6c6b82acb6ee313f1101a7a3aeaa3475a9c9c149
Diego Santana <diego.alves.santana@hotmail.com>
2023-06-17 21:24:15 -0300

    Início da página de pagamento
    Iniciei a inserção da página de pagamento, além de mudar a fonte das letras da página para Arial.

--- public/Routes/rota_teste.js
+++ public/Routes/rota_teste.js
@@ -5,22 +5,10 @@ const Itens = require('../../Models/Itens');
 const { Op } = require('sequelize');
 
 
-
-
-router.get('/teste', (req,res) =>{
-    let codReferencia = req.query.codReferencia;
-    
-    console.log('variavél:' + codReferencia)
-
-    Itens.findAll({where: {ITM_CodReferencia: codReferencia}}).then((itens)=>{
-        if(req.session.usuario){
-            var usuarioSessao = req.session.usuario
-            res.render('item', {itens: itens, usuario: usuarioSessao, redirectUrl: codReferencia})
-        }else{
-            res.render('item', {itens: itens, redirectUrl: codReferencia})
-        }
-    })
+router.get('/teste', (req,res)=>{
+    res.render('teste')
 })
+
 
 
 
